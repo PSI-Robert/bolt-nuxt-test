fae19852edf611ea31a639177d1893d102b523c1
Robert Federico <robertfederico@Roberts-MacBook-Air.local>
2024-11-04 20:57:31 +0800

    initial commit

--- app.vue
+++ app.vue
@@ -1,9 +1,6 @@
 <template>
    <div class="flex min-h-screen bg-slate-50 dark:bg-black dark:text-white">
       <NuxtLayout>
-         <div class="hello" />
-         <Button>Click Me</Button>
-         <Button>Click Me</Button>
          <Button>Click Me</Button>
       </NuxtLayout>
    </div>
