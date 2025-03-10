f3a4ff011fe7baa9829135b3f840b8efecd75c58
PSI-Robert <robertfederico.praxxys@gmail.com>
2025-03-10 10:18:43 +0800

    wip

--- config/env-validator.ts
+++ config/env-validator.ts
@@ -1,7 +1,7 @@
 export function validateEnvironment() {
    const requiredVariables = ['NUXT_PUBLIC_API_BASE', 'API_SECRET', 'NODE_ENV'];
    console.log(process.env.NODE_ENV === 'test');
-
+}
 export const runtimeConfig = {
    public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api',
